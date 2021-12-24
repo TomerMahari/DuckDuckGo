6ca3596c6d538e88b3766a1270b3c3443289ebb5
galit-ws <galit.adane@gmail.com>
2021-12-24 03:44:00 +0200

    notes

--- DuckDuckGoAPI/Startup.cs
+++ DuckDuckGoAPI/Startup.cs
@@ -17,6 +17,7 @@ namespace DuckDuckGoAPI
         {
             services.AddControllers();
 
+            //DI DIP
             services.AddScoped<ISearchEngineRepo, SearchEngineRepo>();
             services.AddControllers();
 
